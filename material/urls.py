0938ecd80bc45cec1a12d0cdcc66524a750cb99b
MaksimVolkov <skazimko@yandex.ru>
2025-01-27 23:04:43 +0300

    32.1

--- material/urls.py
+++ material/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CourseViewSet, LessonViewSet
+from .views import CourseViewSet, LessonViewSet, SubscriptionView
 
 app_name = "materials"
 
@@ -8,4 +8,7 @@ router = DefaultRouter()
 router.register(r"courses", CourseViewSet, basename="course")
 router.register(r"lessons", LessonViewSet, basename="lesson")
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [
+    path("", include(router.urls)),
+    path("subscriptions/", SubscriptionView.as_view(), name="subscription"),
+]
