1f2712035a817fd996c0d61f244bbaf972a7a57b
MaksimVolkov <skazimko@yandex.ru>
2025-03-03 21:31:46 +0300

    nginx

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,8 @@ FROM python:3.12
 
 WORKDIR /app
 
-RUN apt-get update && apt-get install -y \
-    gcc \
-    libpq-dev \
+RUN apt-get update \
+    && apt-get install -y gcc libpq-dev \
     && apt-get clean \
     && rm -rf /var/lib/apt/lists/*
 
--- config/settings.py
+++ config/settings.py
@@ -98,7 +98,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
@@ -131,7 +136,7 @@ STRIPE_API_KEY = os.getenv("STRIPE_API_KEY")
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://localhost:6379/1",
+        "LOCATION": "redis://redis:6379/0",
     }
 }
 CELERY_BEAT_SCHEDULE = {
